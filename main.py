8f2db5dbcfcade9a638d1b7b23d17cf37fdcaf35
Nicholas Villarreal <nicholasvillarreal@gmail.com>
2018-10-22 01:25:59 -0600

    fixed pygame hang after display initialization after hack

--- main.py
+++ main.py
@@ -2,18 +2,37 @@ import os
 import pygame
 from time import sleep
 
+from signal import alarm, signal, SIGALRM, SIGKILL
+
+lcd = None
+
+def init_Pygame():
+    global lcd
+    # this section is an unbelievable nasty hack - for some reason Pygame
+    # needs a keyboardinterrupt to initialise in some limited circs
+    class Alarm(Exception):
+        pass
+    def alarm_handler(signum, frame):
+        raise Alarm
+    signal(SIGALRM, alarm_handler)
+    alarm(3)
+    try:
+        pygame.init()
+        print "getting lcd"
+        lcd = pygame.display.set_mode() 
+        alarm(0)
+    except Alarm:
+        raise KeyboardInterrupt
+
 #Colours
 WHITE = (255,255,255)
 
 os.putenv('SDL_VIDEODRIVER', 'fbcon')
 os.putenv('SDL_FBDEV', '/dev/fb1')
 print "Initializing..."
-pygame.init()
+init_Pygame()
 print "hiding mouse"
 pygame.mouse.set_visible(False)
-print (pygame.display.get_init())
-print "getting lcd"
-lcd = pygame.display.set_mode()
 print "filling screen with black"
 lcd.fill((0,0,0))
 print "updating screen"
@@ -26,6 +45,5 @@ while True:
     text_surface = font_big.render('Hello World', True, WHITE)
     rect = text_surface.get_rect(center=(240,160))
     lcd.blit(text_surface, rect)
-    pygame.display.update()
-    print "sleeping..."
+    pygame.display.update()    
     sleep(0.1)    
