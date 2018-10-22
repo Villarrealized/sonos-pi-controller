9e7811aee6e7c540515feb94cc8198f08be65196
Nicholas Villarreal <nicholasvillarreal@gmail.com>
2018-10-22 09:48:13 -0600

    testing fixes

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import os
 import pygame
+from pygame.locals import *
+import os
 from time import sleep
-
+import RPi.GPIO as GPIO
 from signal import alarm, signal, SIGALRM, SIGKILL
 
 lcd = None
-
 def init_Pygame():
     global lcd
     # this section is an unbelievable nasty hack - for some reason Pygame
@@ -24,25 +24,56 @@ def init_Pygame():
     except Alarm:
         raise KeyboardInterrupt
 
+#Setup the GPIOs as outputs - only 4 and 17 are available
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.OUT)
+GPIO.setup(17, GPIO.OUT)
+
 #Colours
 WHITE = (255,255,255)
 
-print "Initializing..."
-init_Pygame()
+#init_Pygame()
+pygame.init()
+print "getting lcd"
+lcd = pygame.display.set_mode() 
 
-print "hiding mouse"
 pygame.mouse.set_visible(False)
-print "filling screen with black"
 lcd.fill((0,0,0))
-print "updating screen"
 pygame.display.update()
 
-font_big = pygame.font.Font(None, 100)
+font_big = pygame.font.Font(None, 50)
 
-print "entering main loop...."
-while True:
-    text_surface = font_big.render('Hello World', True, WHITE)
-    rect = text_surface.get_rect(center=(240,160))
+touch_buttons = {'17 on':(80,60), '4 on':(240,60), '17 off':(80,180), '4 off':(240,180)}
+
+for k,v in touch_buttons.items():
+    text_surface = font_big.render('%s'%k, True, WHITE)
+    rect = text_surface.get_rect(center=v)
     lcd.blit(text_surface, rect)
-    pygame.display.update()    
-    sleep(0.1)    
+
+pygame.display.update()
+
+print "entering main loop"
+while True:
+    # Scan touchscreen events
+    for event in pygame.event.get():
+        if(event.type is MOUSEBUTTONDOWN):
+            pos = pygame.mouse.get_pos()
+            print pos
+        elif(event.type is MOUSEBUTTONUP):
+            pos = pygame.mouse.get_pos()
+            print pos
+            #Find which quarter of the screen we're in
+            x,y = pos
+            if y < 160:
+                if x < 240:
+                    print "off"
+                    GPIO.output(17, False)
+                else:
+                    GPIO.output(4, False)
+            else:
+                if x < 240:
+                    print "on"
+                    GPIO.output(17, True)
+                else:
+                    GPIO.output(4, True)
+    sleep(0.1)
