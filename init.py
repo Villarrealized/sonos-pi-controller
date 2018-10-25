3d86f8f78c4029ebfa63700bf08c0a04d566fd0f
Nicholas Villarreal <nicholasvillarreal@gmail.com>
2018-10-25 00:21:42 -0600

    organizing and separating into multiple files

--- init.py
+++ init.py
@@ -1,8 +1,12 @@
 from signal import alarm, signal, SIGALRM, SIGKILL
+
 import pygame
+
+from backlight import Backlight
+import color
 # return the pygame screen
 def init_pygame():
-    global lcd
+    backlight = Backlight()
     # this section is an unbelievable nasty hack - for some reason Pygame
     # needs a keyboardinterrupt to initialise in some limited circs
     class Alarm(Exception):
@@ -15,6 +19,19 @@ def init_pygame():
         pygame.init()
         print "getting lcd"
         lcd = pygame.display.set_mode()
+        # Hide the mouse
+        pygame.mouse.set_visible(False)        
+        # Set the background color and turn on display
+        backlight.on()
+        lcd.fill(color.NAVY)
+        pygame.display.update()
+
+        font_big = pygame.font.Font(None, 30)
+        text_surface = font_big.render('Tap to pause/play music in TV Room', True, color.WHITE)
+        rect = text_surface.get_rect(center=(240,160))
+        lcd.blit(text_surface, rect)
+        pygame.display.update()
+
         alarm(0)
         return lcd
     except Alarm:
--- main.py
+++ main.py
@@ -2,47 +2,35 @@ import os
 import signal
 import sys
 from time import sleep
+
 import pygame
 from pygame.locals import *
 import pygameui
+from init import init_pygame
 import requests
 
-from init import init_pygame
+# import project files
+import color
+from backlight import Backlight
 
+# Environment Vars
 API_URL = os.getenv('API_URL')
-BACKLIGHT_CONTROL = os.getenv('BACKLIGHT_CONTROL')
 
-# Turn the backlight on
-with open(BACKLIGHT_CONTROL, 'w') as f:
-    f.write('1')
+# Global Vars
+lcd = None
+playing = False
+backlight = Backlight()
 
+# Initialize pygame
 lcd = init_pygame()
 
-#Colours
-BLACK = (0,0,0)
-WHITE = (255,255,255)
-NAVY = (11,64,109)
-
-pygame.mouse.set_visible(False)
-lcd.fill(NAVY)
-pygame.display.update()
-
-font_big = pygame.font.Font(None, 30)
-text_surface = font_big.render('Tap to pause/play music in TV Room', True, WHITE)
-rect = text_surface.get_rect(center=(240,160))
-lcd.blit(text_surface, rect)
-pygame.display.update()
-
-playing = False
-
-
+# Handle Terminate signal, exit gracefully.
 def exit_app(sig, frame):
     print "Clearing screen..."
-    lcd.fill(BLACK)
+    lcd.fill(color.BLACK)
     pygame.display.update()
     print "Turning off backlight..."
-    with open(BACKLIGHT_CONTROL, 'w') as f:
-                f.write('0')
+    backlight.off()
     sys.exit(0)
 # Register signal
 signal.signal(signal.SIGTERM, exit_app)
@@ -60,7 +48,7 @@ while True:
                 playing = True
             pos = pygame.mouse.get_pos()
             print pos
-    sleep(0.1)
+    sleep(0.02)
 
 
     
